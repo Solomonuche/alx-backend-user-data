5938abf89085d84cb0c9e066b94a48999d9aa053
Solomon <solomonuche@gmail.com>
2024-05-16 15:34:47 +0100

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,13 +3,16 @@
 Regex-ing module
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
     """
     a function that returns a log message obfuscated
     """
-
     for field in fields:
         pattern = rf'{field}=([^;]*)'
         message = re.sub(pattern, f"{field}={redaction}", message)
