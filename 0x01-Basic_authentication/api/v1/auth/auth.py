7d1eb69c8405afe08ee18d01c786567d1a2955e5
Solomon <solomonuche@gmail.com>
2024-05-22 08:54:26 +0100

    excluded_paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,7 +16,11 @@ class Auth():
         authentication required
         """
 
-        return False
+        if not path or not excluded_paths:
+            return True
+        if path in excluded_paths or f"{path}/" in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
