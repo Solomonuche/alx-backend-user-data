a81c6910aaeb67319742adb7a73cdc90c56c4134
Solomon <solomonuche@gmail.com>
2024-05-22 17:59:03 +0100

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,13 @@ class BasicAuth(Auth):
     """
     BasicAuth class
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Basic - Base64 part
+        """
+        if not authorization_header or type(authorization_header) != str:
+            return None
+        if authorization_header.split()[0] != "Basic":
+            return None
+        return authorization_header.split()[1]
