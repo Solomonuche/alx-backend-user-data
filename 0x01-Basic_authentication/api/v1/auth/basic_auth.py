a3fa8c7dc029ad8573aad96ed6e59adc1e880438
Solomon <solomonuche@gmail.com>
2024-05-22 17:02:58 +0100

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic authentication implementation
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class
+    """
+    pass
