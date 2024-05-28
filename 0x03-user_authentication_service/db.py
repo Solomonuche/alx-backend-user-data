ed4705f7c54b9667d9b07e89d702bcdb3fe22961
Solomon <solomonuche@gmail.com>
2024-05-28 14:11:23 +0100

    create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,9 +34,7 @@ class DB:
         """
         create a user instance
         """
-        user = User()
-        user.email = email
-        user.hashed_password = hashed_password
+        user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
