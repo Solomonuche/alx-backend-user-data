b2b69cf12812a099b276a4274677d745a0dc3d90
Solomon <solomonuche@gmail.com>
2024-05-29 15:52:27 +0100

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -43,3 +43,22 @@ class Auth:
             user = self._db.add_user(email, _hash_password(password))
 
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Credentials validation
+        """
+        if not email or not isinstance(email, str):
+            raise False
+        if not password or not isinstance(password, str):
+            return False
+
+        user = None
+        try:
+            user = self._db.find_user_by(email=email)
+        except Exception:
+            pass
+        if user:
+            passwd = password.encode('utf-8')
+            return bcrypt.checkpw(passwd, user.hashed_password)
+        return False
