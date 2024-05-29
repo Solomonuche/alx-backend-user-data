ccb59348fd90748807e7adee150f5ddc6b87a0a1
Solomon <solomonuche@gmail.com>
2024-05-29 19:15:58 +0100

    Find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,7 +87,7 @@ class Auth:
 
         return session_id
 
-    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+    def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
         """
         Find user by session ID
         """
